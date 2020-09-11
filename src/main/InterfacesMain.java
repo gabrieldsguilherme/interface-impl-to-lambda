6c72b88c11ae2ef444497f38c217a1e1ba636cfd
Gabriel Guilherme <gabriel.guilherme@sensedia.com>
2020-09-11 15:34:50 -0300

    Adding todos

--- src/main/InterfacesMain.java
+++ src/main/InterfacesMain.java
@@ -9,6 +9,12 @@ public class InterfacesMain {
         List<Integer> list1 = Arrays.asList(2, 1, 3, 5, 7, 10, 4, 9, 6, 8);
         List<Integer> list2 = Arrays.asList(2, 1, 3, 5, 7, 10, 4, 9, 6, 8);
 
+        // TODO: Sort list1 asc
+
+        // TODO: Sort list2 desc
+
+        // TODO: filter odd numbers from any list
+
         System.out.println("Asc: " + list1);
         System.out.println("Desc: " + list2);
     }
