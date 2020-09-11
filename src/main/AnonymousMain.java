1a21fe6fa26405a3b9b2f82f4bdcaea96863f2e1
Gabriel Guilherme <gabriel.guilherme@sensedia.com>
2020-09-11 15:32:35 -0300

    Adding more elements to lists

--- src/main/AnonymousMain.java
+++ src/main/AnonymousMain.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class AnonymousMain {
 
     public static void main(String[] args) {
-        List<Integer> list1 = Arrays.asList(2, 1, 3);
-        List<Integer> list2 = Arrays.asList(2, 1, 3);
+        List<Integer> list1 = Arrays.asList(2, 1, 3, 5, 7, 10, 4, 9, 6, 8);
+        List<Integer> list2 = Arrays.asList(2, 1, 3, 5, 7, 10, 4, 9, 6, 8);
 
         System.out.println("Asc: " + list1);
         System.out.println("Desc: " + list2);
