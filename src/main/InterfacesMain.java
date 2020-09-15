e8af62b6341d07133314409d8a22c8fb74d93e51
Gabriel Guilherme <gabriel.guilherme@sensedia.com>
2020-09-15 18:08:00 -0300

    Using Arrays.asList

--- src/main/InterfacesMain.java
+++ src/main/InterfacesMain.java
@@ -1,12 +1,13 @@
 package main;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class InterfacesMain {
 
     public static void main(String[] args) {
-        List<Integer> list1 = List.of(2, 1, 3, 5, 7, 10, 4, 9, 6, 8);
-        List<Integer> list2 = List.of(2, 1, 3, 5, 7, 10, 4, 9, 6, 8);
+        List<Integer> list1 = Arrays.asList(2, 1, 3, 5, 7, 10, 4, 9, 6, 8);
+        List<Integer> list2 = Arrays.asList(2, 1, 3, 5, 7, 10, 4, 9, 6, 8);
 
         // TODO: Sort list1 asc
 
